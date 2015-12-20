0d8b586de13787bc052ffb0b762bbafbe58dee5c
Yishen Chang <samkami@icloud.com>
2015-12-20 18:44:26 +0800

    Initial Commit

---
+++ Ater/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ater
+//
+//  Created by 張楹翔 on 2015/12/20.
+//  Copyright © 2015年 張楹翔. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
