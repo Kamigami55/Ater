e837164df57dee75737486a9729890cc7af44c26
Yishen Chang <samkami@icloud.com>
2015-12-22 16:46:47 +0800

    Add editor controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
+  get 'editor' => 'editor#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
